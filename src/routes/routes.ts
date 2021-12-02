a0f3d0de77b678679de53b4417e234bb2b94f8e1
CNoeCardenasccardenas@hellobuild.coccardenas@hellobuild.coccardenas@hellobuild.co <ccardenas@hellobuild.co>
2021-12-01 21:14:12 -0500

    implements lazy load

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,28 +1,32 @@
-import { LazyPage1, LazyPage2, LazyPage3 } from "../lazy-load-module/pages";
+import { lazy, LazyExoticComponent } from "react";
 
+type JSXComponent = () => JSX.Element
 interface Route {
         path: string,
         to: string,
-        Component: () => JSX.Element,
+        Component: LazyExoticComponent<JSXComponent>  | JSXComponent,
         name: string
 }
 
+const Lazy1 = lazy(()=> import(/* webpackChunkName: "lazy1" */'../lazy-load-module/pages/LazyPage1'))
+const Lazy2 = lazy(()=> import(/* webpackChunkName: "lazy2" */'../lazy-load-module/pages/LazyPage2'))
+const Lazy3 = lazy(()=> import(/* webpackChunkName: "lazy3" */'../lazy-load-module/pages/LazyPage3'))
 
 export const routes: Route[] = [
     {
         path: 'lazy1',
         to: '/lazy1',
-        Component: LazyPage1,
+        Component: Lazy1,
         name: 'Lazy-1'
     },{
         path: 'lazy2',
         to: '/lazy2',
-        Component: LazyPage2,
+        Component: Lazy2,
         name: 'Lazy-2'
     },{
         path: 'lazy3',
         to: '/lazy3',
-        Component: LazyPage3,
+        Component: Lazy3,
         name: 'Lazy-3'
     }
 ]
